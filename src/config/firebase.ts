89810f8c988da5c76afffc8fdab2b9a658f4001f
dlvinn <115410984+dlvinn@users.noreply.github.com>
2023-12-18 14:58:37 +0300

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,12 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDSNDR5Ldy-ROSYWTruARC0mXhhSTF9zJg",
-  authDomain: "react-course-1be72.firebaseapp.com",
-  projectId: "react-course-1be72",
-  storageBucket: "react-course-1be72.appspot.com",
-  messagingSenderId: "600874545728",
-  appId: "1:600874545728:web:00ebeba2a3df3881d7a5a5"
+  
 };
 
 // Initialize Firebase
